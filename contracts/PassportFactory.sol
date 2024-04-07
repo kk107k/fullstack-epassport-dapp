3b17f59c955bd2583ccdaa551f7e46ea3a9a5344
K Kiasat <kk107@hw.ac.uk>
2024-04-07 15:48:53 +0400

    Update PassportFactory.sol

--- contracts/PassportFactory.sol
+++ contracts/PassportFactory.sol
@@ -12,11 +12,11 @@ contract PassportFactory {
         string name;
         string passportNumber;
         string nationality;
-        uint256 birthDate;
+        string birthDate;
         string placeOfBirth;
         string sex;
-        uint256 issueDate;
-        uint256 expiryDate;
+        string issueDate;
+        string expiryDate;
     }
 
     mapping(address => PassportDetails) public passportDetailsMap;
@@ -31,65 +31,97 @@ contract PassportFactory {
     }
 
     function addPassport(
-        string memory _name,
-        string memory _passportNumber,
-        string memory _nationality,
-        uint256 _birthDate,
-        string memory _placeOfBirth,
-        string memory _sex,
-        uint256 _issueDate,
-        uint256 _expiryDate
-    ) public onlyOwner {
-        PassportRegistry newPassportContract = new PassportRegistry();
-        newPassportContract.addPassport(
-            _name,
-            _passportNumber,
-            _nationality,
-            _birthDate,
-            _placeOfBirth,
-            _sex,
-            _issueDate,
-            _expiryDate
-        );
-        address passportAddress = address(newPassportContract);
-        passportContracts.push(passportAddress);
-        
-        passportDetailsMap[passportAddress] = PassportDetails(
-            passportAddress,
-            _name,
-            _passportNumber,
-            _nationality,
-            _birthDate,
-            _placeOfBirth,
-            _sex,
-            _issueDate,
-            _expiryDate
-        );
+    string memory _name,
+    string memory _passportNumber,
+    string memory _nationality,
+    string memory _birthDate,
+    string memory _placeOfBirth,
+    string memory _sex,
+    string memory _issueDate,
+    string memory _expiryDate
+) public onlyOwner {
+    uint256 birthDateTimestamp = stringToTimestamp(_birthDate);
+    uint256 issueDateTimestamp = stringToTimestamp(_issueDate);
+    uint256 expiryDateTimestamp = stringToTimestamp(_expiryDate);
+
+    PassportRegistry newPassportContract = new PassportRegistry();
+    newPassportContract.addPassport(
+        _name,
+        _passportNumber,
+        _nationality,
+        birthDateTimestamp, // Pass uint256 instead of string
+        _placeOfBirth,
+        _sex,
+        issueDateTimestamp, // Pass uint256 instead of string
+        expiryDateTimestamp // Pass uint256 instead of string
+    );
+    address passportAddress = address(newPassportContract);
+    passportContracts.push(passportAddress);
+
+    passportDetailsMap[passportAddress] = PassportDetails(
+        passportAddress,
+        _name,
+        _passportNumber,
+        _nationality,
+        _birthDate,
+        _placeOfBirth,
+        _sex,
+        _issueDate,
+        _expiryDate
+    );
+}
+
+    function stringToTimestamp(string memory _date) internal pure returns (uint256) {
+        bytes memory b = bytes(_date);
+        require(b.length == 10, "Invalid date format");
+        uint256 year = parseInt(_substring(b, 6, 4));
+        uint256 month = parseInt(_substring(b, 0, 2));
+        uint256 day = parseInt(_substring(b, 3, 2));
+        require(year >= 1970, "Year must be greater than or equal to 1970");
+
+        return _toTimestamp(year, month, day);
     }
 
-    function getPassport(address _passportAddress) public view returns (
-        string memory name,
-        string memory passportNumber,
-        string memory nationality,
-        uint256 birthDate,
-        string memory placeOfBirth,
-        string memory sex,
-        uint256 issueDate,
-        uint256 expiryDate,
-        address ePassportAddress
-    ) {
-        PassportDetails memory details = passportDetailsMap[_passportAddress];
-        return (
-            details.name,
-            details.passportNumber,
-            details.nationality,
-            details.birthDate,
-            details.placeOfBirth,
-            details.sex,
-            details.issueDate,
-            details.expiryDate,
-            details.passportAddress
-        );
+    function parseInt(string memory _a) internal pure returns (uint256) {
+        bytes memory bresult = bytes(_a);
+        uint256 mint = 0;
+        bool decimals = false;
+        for (uint256 i = 0; i < bresult.length; i++) {
+            if ((uint256(uint8(bresult[i])) >= 48) && (uint256(uint8(bresult[i])) <= 57)) {
+                if (decimals) {
+                    break;
+                }
+                mint *= 10;
+                mint += uint256(uint8(bresult[i])) - 48;
+            } else if (uint256(uint8(bresult[i])) == 46) {
+                decimals = true;
+            } else {
+                revert();
+            }
+        }
+        return mint;
+    }
+
+    function _substring(bytes memory _a, uint256 _startIndex, uint256 _endIndex) internal pure returns (string memory) {
+        require(_startIndex <= _endIndex && _endIndex < _a.length, "Invalid substring indices");
+        bytes memory result = new bytes(_endIndex - _startIndex + 1);
+        for (uint256 i = _startIndex; i <= _endIndex; i++) {
+            result[i - _startIndex] = _a[i];
+        }
+        return string(result);
+    }
+
+    function _toTimestamp(uint256 _year, uint256 _month, uint256 _day) internal pure returns (uint256 timestamp) {
+        uint256 c = 0;
+        if (_month <= 2) {
+            _year--;
+            _month += 12;
+        }
+        uint256 _e = _year / 100;
+        uint256 _f = 2 - _e + (_e / 4);
+        c = 36525 * (_year + 4716) / 100;
+        uint256 d = 306 * (_month + 1) / 10;
+        return (c + d + _day + _f - 1524) * 86400 - 10800;
     }
 
     function deletePassport(uint256 index) public onlyOwner {
@@ -97,19 +129,15 @@ contract PassportFactory {
         delete passportContracts[index];
     }
 
-    function updatePassportDate(uint256 id, uint256 _issueDate, uint256 _expiryDate) public onlyOwner {
-    require(id < passportContracts.length, "Invalid index");
-    PassportRegistry passportContract = PassportRegistry(passportContracts[id]);
-    passportContract.updatePassportDates(id, _issueDate, _expiryDate);
+    function updatePassportDate(uint256 id, string memory _issueDate, string memory _expiryDate) public onlyOwner {
+        require(id < passportContracts.length, "Invalid index");
+        PassportRegistry passportContract = PassportRegistry(passportContracts[id]);
+        passportContract.updatePassportDates(id, stringToTimestamp(_issueDate), stringToTimestamp(_expiryDate));
 
-    // Update passport details in passportDetailsMap
-    PassportDetails storage details = passportDetailsMap[passportContracts[id]];
-    details.issueDate = _issueDate;
-    details.expiryDate = _expiryDate;
-}
-
-
-
+        PassportDetails storage details = passportDetailsMap[passportContracts[id]];
+        details.issueDate = _issueDate;
+        details.expiryDate = _expiryDate;
+    }
 
     function getPassportContracts() public view returns (address[] memory) {
         return passportContracts;
